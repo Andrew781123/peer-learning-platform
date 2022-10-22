a4fc49b0a5071ac51171769a73d789f10f0d9e25
Andrew781123 <leechihoandrew1123@gmail.com>
2022-10-22 17:09:08 +0800

    file structure

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint/eslint-plugin", "prettier", "import"],
+  extends: [
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: [".eslintrc.js", "/dist/*"],
+  rules: {
+    quotes: ["warn", "double"],
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-empty-function": "warn",
+    "@typescript-eslint/ban-types": "off",
+    "prettier/prettier": [
+      "warn",
+      { singleQuote: false },
+      { endOfLine: "auto" },
+    ],
+    "import/no-relative-parent-imports": "error",
+    "import/no-unresolved": "off",
+  },
+};
