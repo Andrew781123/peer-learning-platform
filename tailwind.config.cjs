2bd90f005be60cf6e35025726ed16c1da73fe009
Andrew781123 <leechihoandrew1123@gmail.com>
2022-10-24 18:52:28 +0800

    custom color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          default: "#90caf9",
+          dark: "#5d99c6",
+          light: "#c3fdff",
+        },
+        onPrimary: "#000000",
+        secondary: {
+          default: "#ffab91",
+          dark: "#c97b63",
+          light: "#ffddc1",
+        },
+        onSecondary: "#000000",
+        background: "#121212",
+        onBackground: "#ffffff",
+        surface: "#1f1f1f",
+        onSurface: "#ffffff",
+      },
+    },
   },
   plugins: [],
 };
