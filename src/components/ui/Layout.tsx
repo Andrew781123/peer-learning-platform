64dd05a2c3b87c2c1baa58da42164356671b4862
Andrew781123 <leechihoandrew1123@gmail.com>
2022-10-25 15:43:49 +0800

    changed layout

--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -10,7 +10,7 @@ const Layout = (props: LayoutProps) => {
   return (
     <>
       <Navbar />
-      <main className="p-5 text-onBackground">{children}</main>
+      <main className="m-auto max-w-3xl p-5 text-onBackground">{children}</main>
     </>
   );
 };
