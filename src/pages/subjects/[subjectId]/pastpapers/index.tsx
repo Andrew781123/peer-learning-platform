cfebe631faade48632e97fd458c59dcf6c1276da
Andrew781123 <leechihoandrew1123@gmail.com>
2022-10-25 12:07:46 +0800

    generate static paths for pastpaper

---
+++ src/pages/subjects/[subjectId]/pastpapers/index.tsx
@@ -0,0 +1,72 @@
+import { PrismaClient } from "@prisma/client";
+import { createProxySSGHelpers } from "@trpc/react/ssg";
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next";
+import { ParsedUrlQuery } from "querystring";
+import superjson from "superjson";
+import PageHeader from "../../../../components/ui/PageHeader";
+import { createContextInner } from "../../../../server/trpc/context";
+import { appRouter } from "../../../../server/trpc/router/_app";
+import { trpc } from "../../../../utils/trpc";
+
+interface IParams extends ParsedUrlQuery {
+  subjectId: string;
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prisma = new PrismaClient();
+
+  const subjects = await prisma.subject.findMany();
+
+  const paths = subjects.map((subject) => ({
+    params: { subjectId: subject.id },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (
+  context: GetStaticPropsContext
+) => {
+  const ssg = createProxySSGHelpers({
+    router: appRouter,
+    ctx: await createContextInner({ session: null }),
+    transformer: superjson,
+  });
+
+  const params = context.params as IParams;
+
+  await ssg.pastPaper.getAllBySubject.prefetch({ subjectId: params.subjectId });
+
+  return {
+    props: {
+      subjectId: params.subjectId,
+      trpcState: ssg.dehydrate(),
+    },
+    // No need to revalidate, we don't have any dynamic data
+  };
+};
+
+type PastPaperPageProps = {
+  subjectId: string;
+};
+
+const PastPaperPage = (props: PastPaperPageProps) => {
+  const { subjectId } = props;
+
+  const pastPapers = trpc.pastPaper.getAllBySubject.useQuery({
+    subjectId,
+  });
+
+  console.log({ data: pastPapers.data });
+
+  return (
+    <div>
+      <PageHeader title="Past Papers" />
+    </div>
+  );
+};
+
+export default PastPaperPage;
