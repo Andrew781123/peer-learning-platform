6a24ac6bfb272bc9e1a1f5049d874c9f81bae41c
Andrew781123 <leechihoandrew1123@gmail.com>
2022-10-27 22:29:41 +0800

    question response with solutionCount and diffRating

--- src/server/trpc/router/question.ts
+++ src/server/trpc/router/question.ts
@@ -35,12 +35,34 @@ export const questionRouter = router({
         },
       });
 
+      const questionReports = await ctx.prisma.questionSolution.groupBy({
+        by: ["questionId"],
+        where: {
+          question: {
+            pastPaperId: input.pastPaperId,
+          },
+        },
+        _avg: {
+          difficultyRating: true,
+        },
+        _count: true,
+      });
+
+      console.log({ questionReports });
+
+      const questionReportsMap = new Map(
+        questionReports.map((report) => [report.questionId, report])
+      );
+
       const response = questions.map((question) => ({
         ...question,
         topics: question.topics
           .filter((topic) => topic.topic._count.questions >= MIN_TAG_COUNT)
           .sort((a, b) => b.topic._count.questions - a.topic._count.questions)
           .map((topic) => topic.topic.name),
+        solutionCount: questionReportsMap.get(question.id)?._count ?? 0,
+        difficultyRating:
+          questionReportsMap.get(question.id)?._avg.difficultyRating ?? 0,
       }));
 
       return response;
