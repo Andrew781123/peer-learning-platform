a6986269035ac64d7630526ecf5a765e0263cd8b
Andrew781123 <leechihoandrew1123@gmail.com>
2022-10-27 20:48:56 +0800

    serialise question query

--- src/server/trpc/router/question.ts
+++ src/server/trpc/router/question.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 import { publicProcedure, router } from "../trpc";
 
+const MIN_TAG_COUNT = 2;
+
 export const questionRouter = router({
   getAllByPastPaper: publicProcedure
     .input(
@@ -8,9 +10,36 @@ export const questionRouter = router({
         pastPaperId: z.number(),
       })
     )
-    .query(({ input, ctx }) => {
-      return ctx.prisma.question.findMany({
+    .query(async ({ input, ctx }) => {
+      const questions = await ctx.prisma.question.findMany({
         where: { pastPaperId: input.pastPaperId },
+        select: {
+          id: true,
+          number: true,
+          topics: {
+            where: {
+              tagCount: {
+                gt: MIN_TAG_COUNT,
+              },
+            },
+            select: {
+              id: true,
+              topic: {
+                select: {
+                  id: true,
+                  name: true,
+                },
+              },
+            },
+          },
+        },
       });
+
+      const response = questions.map((question) => ({
+        ...question,
+        topics: question.topics.map((topic) => topic.topic.name),
+      }));
+
+      return response;
     }),
 });
