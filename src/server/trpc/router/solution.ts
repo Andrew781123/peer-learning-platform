966882f1566e684ca251200e56cb24f63e91a7cc
Andrew781123 <leechihoandrew1123@gmail.com>
2022-11-20 08:33:55 +0800

    update id to autoincrement

--- src/server/trpc/router/solution.ts
+++ src/server/trpc/router/solution.ts
@@ -4,14 +4,27 @@ import { publicProcedure, router } from "../trpc";
 export const solutionRouter = router({
   createMany: publicProcedure
     .input(
-      z.array(
-        z.object({
-          subjectId: z.string(),
-          questionId: z.number(),
-          markdown: z.string(),
-          difficultyRatingId: z.number(),
-        })
-      )
+      z.object({
+        pastPaperId: z.number(),
+        solutions: z.array(
+          z.object({
+            number: z.number(),
+            pastPaperId: z.number(),
+            questionId: z.number(),
+            markdown: z.string(),
+            difficultyRatingId: z.number(),
+          })
+        ),
+      })
     )
-    .mutation(({ input, ctx }) => {}),
+    .mutation(({ input, ctx }) => {
+      const { pastPaperId, solutions } = input;
+
+      ctx.prisma.question.createMany({
+        data: solutions.map((solution) => ({
+          number: solution.number,
+          pastPaperId,
+        })),
+      });
+    }),
 });
