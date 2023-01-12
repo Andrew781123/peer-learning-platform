70624cf5bd7807d7a6d51af2ea030d250dbf1396
Andrew781123 <leechihoandrew1123@gmail.com>
2023-01-12 09:47:08 +0800

    refactor question service

--- src/server/trpc/router/question.ts
+++ src/server/trpc/router/question.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import getDifficultyLevel from "../../utils/getDifficultyRating";
+import { getAllQuestionsByPastPaper } from "../../../services/question-service";
 import { publicProcedure, router } from "../trpc";
 
 const MIN_TAG_COUNT = 0;
@@ -12,74 +12,17 @@ export const questionRouter = router({
       })
     )
     .query(async ({ input, ctx }) => {
-      const questions = await ctx.prisma.question.findMany({
-        where: {
-          pastPaperId: input.pastPaperId,
-        },
-        select: {
-          id: true,
-          number: true,
-          topics: {
-            select: {
-              topic: {
-                select: {
-                  name: true,
-                  _count: {
-                    select: {
-                      questions: true,
-                    },
-                  },
-                },
-              },
-            },
-          },
-          solutions: {
-            select: {
-              difficultyRating: {
-                select: {
-                  value: true,
-                },
-              },
-            },
-          },
-          _count: {
-            select: {
-              solutions: true,
-            },
-          },
-        },
-      });
-
-      const getAverageDifficultyRatingScore = (
-        question: typeof questions[0]
-      ): number => {
-        return (
-          question.solutions.reduce(
-            (sum, solution) => (sum += solution.difficultyRating?.value ?? 0),
-            0
-          ) / (question.solutions.length || 1)
-        );
-      };
+      const questions = await getAllQuestionsByPastPaper(
+        ctx.prisma.question,
+        input.pastPaperId
+      );
 
       const response = questions.map((question) => ({
         ...question,
-        topics: Object.entries(
-          question.topics.reduce((acc, topic) => {
-            if (topic.topic.name in acc) return acc;
-            if (topic.topic._count.questions < MIN_TAG_COUNT) return acc;
-            return {
-              ...acc,
-              [topic.topic.name]: topic.topic._count.questions,
-            };
-          }, {} as { [key: string]: number })
-        )
-          .map(([name, count]) => ({ name, count }))
+        topics: question.topics
+          .filter((topic) => topic.count > MIN_TAG_COUNT)
           .sort((a, b) => b.count - a.count)
           .map((topic) => topic.name),
-        solutionCount: question._count.solutions,
-        difficultyLevel: getDifficultyLevel(
-          getAverageDifficultyRatingScore(question)
-        ),
       }));
 
       return response;
--- src/services/question-service.ts
+++ src/services/question-service.ts
@@ -1,21 +1,20 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Question } from "@prisma/client";
 import { DifficultyLevel } from "../constants/difficultyRating";
 import getDifficultyLevel from "../server/utils/getDifficultyRating";
 import { getAverageDifficultyRatingScore } from "../utils/question";
 
-export const getAllByPastPaper = async (
+type FormattedQuestion = {
+  topics: {
+    name: string;
+    count: number;
+  }[];
+  solutionCount: number;
+  difficultyLevel: DifficultyLevel;
+} & Omit<Question, "pastPaperId">;
+export const getAllQuestionsByPastPaper = async (
   repo: PrismaClient["question"],
   pastPaperId: number
-): Promise<
-  {
-    topics: {
-      name: string;
-      count: number;
-    }[];
-    solutionCount: number;
-    difficultyLevel: DifficultyLevel;
-  }[]
-> => {
+): Promise<FormattedQuestion[]> => {
   const questions = await repo.findMany({
     where: {
       pastPaperId: pastPaperId,
@@ -54,24 +53,45 @@ export const getAllByPastPaper = async (
     },
   });
 
-  const formattedQuestions = questions.map((question) => ({
-    ...questions,
-    topics: Object.entries(
-      question.topics.reduce((acc, topic) => {
-        if (topic.topic.name in acc) return acc;
-        return {
-          ...acc,
-          [topic.topic.name]: topic.topic._count.questions,
-        };
-      }, {} as { [key: string]: number })
-    )
-      .map(([name, count]) => ({ name, count }))
-      .sort((a, b) => b.count - a.count),
-    solutionCount: question._count.solutions,
-    difficultyLevel: getDifficultyLevel(
-      getAverageDifficultyRatingScore(question.solutions)
-    ),
-  }));
+  const formattedQuestions = questions.map(formatQuestion);
 
   return formattedQuestions;
 };
+
+const formatQuestion = (question: {
+  number: number;
+  id: string;
+  solutions: {
+    difficultyRating: {
+      value: number;
+    };
+  }[];
+  topics: {
+    topic: {
+      name: string;
+      _count: {
+        questions: number;
+      };
+    };
+  }[];
+  _count: {
+    solutions: number;
+  };
+}): FormattedQuestion => ({
+  ...question,
+  topics: Object.entries(
+    question.topics.reduce((acc, topic) => {
+      if (topic.topic.name in acc) return acc;
+      return {
+        ...acc,
+        [topic.topic.name]: topic.topic._count.questions,
+      };
+    }, {} as { [key: string]: number })
+  )
+    .map(([name, count]) => ({ name, count }))
+    .sort((a, b) => b.count - a.count),
+  solutionCount: question._count.solutions,
+  difficultyLevel: getDifficultyLevel(
+    getAverageDifficultyRatingScore(question.solutions)
+  ),
+});
