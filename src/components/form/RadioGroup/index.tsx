219c8ebb6dc42daa7fca3779335f71c6bea23da9
Andrew781123 <leechihoandrew1123@gmail.com>
2023-02-09 17:17:33 +0800

    fix: radio button colors

--- src/components/form/RadioGroup/index.tsx
+++ src/components/form/RadioGroup/index.tsx
@@ -19,6 +19,21 @@ const RadioGroup = forwardRef<HTMLInputElement, RadioGroupProps>(
 
     return (
       <div>
+        <div className="mb-4 flex items-center">
+          <input
+            id="default-radio-1"
+            type="radio"
+            value=""
+            name="default-radio"
+            className="h-4 w-4 dark:border-gray-600 dark:bg-red-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+          />
+          <label
+            htmlFor="default-radio-1"
+            className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            Default radio
+          </label>
+        </div>
         <div className="relative flex flex-wrap">
           {radios.map((radio) => (
             <div key={radio.label} className="mr-4 flex items-center">
@@ -32,7 +47,7 @@ const RadioGroup = forwardRef<HTMLInputElement, RadioGroupProps>(
                   error
                     ? "border-red-500 text-red-500"
                     : "border-gray-300 text-primary-default",
-                  "h-4 w-4 bg-gray-100"
+                  "h-4 w-4 bg-gray-500"
                 )}
               />
               <label
