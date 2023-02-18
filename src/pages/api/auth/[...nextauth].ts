4884a2e3e475dd6cf71da3e64589c8a350984950
Andrew781123 <leechihoandrew1123@gmail.com>
2023-02-18 22:33:46 +0800

    debug: log headers

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -63,6 +63,7 @@ export const authOptions: NextAuthOptions = {
 const authHandler = NextAuth(authOptions);
 
 export default function auth(req: NextApiRequest, res: NextApiResponse) {
+  console.log("headers", req.headers);
   console.log("authHandler", req.method, req.url);
   // Workaround for known email scanners that send GET or HEAD requests which have
   // the effect of cancelling the one time token. We have seen:
