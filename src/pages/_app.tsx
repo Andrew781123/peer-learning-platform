c5554cdff5a044d1532ef3e35f95c9d30daa8661
Andrew781123 <leechihoandrew1123@gmail.com>
2023-02-23 22:27:35 +0800

    debug: log env

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,6 +17,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  console.log(process.env.ENVIRONMENT);
   return (
     <SessionProvider session={session}>
       <ImageModalContextProvider>
