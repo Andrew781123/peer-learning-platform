9c6a512ac950f08b239fffb1ec1d1bd05b901a75
Andrew781123 <leechihoandrew1123@gmail.com>
2023-02-24 19:18:05 +0800

    refactor: extract solution info

---
+++ src/features/solution/components/solutionDetail/SolutionInfo.tsx
@@ -0,0 +1,46 @@
+import { title } from "@uiw/react-md-editor";
+import clsx from "clsx";
+import { FaUserAlt } from "react-icons/fa";
+
+import { useUser } from "@/hooks/useUser";
+import { getTimeFromX } from "@/server/utils/dates";
+
+type SolutionInfoProps = {
+  solutionName: string;
+  authorId: string;
+  createdAt: Date;
+};
+
+export const SolutionInfo = ({
+  solutionName,
+  authorId,
+  createdAt,
+}: SolutionInfoProps) => {
+  const { user } = useUser();
+
+  return (
+    <>
+      <p className="text-gray-400">
+        from <span className="text-gray-300">{solutionName}</span>
+      </p>
+
+      <p className="text-gray-400">
+        posted{" "}
+        <span className="text-gray-300">
+          {getTimeFromX({ toDate: new Date(createdAt) })}
+        </span>
+      </p>
+
+      <div
+        className={clsx(
+          user?.id !== authorId && "hidden",
+          "flex items-center space-x-1 text-yellow-300"
+        )}
+      >
+        <FaUserAlt />
+        <span>You are the </span>
+        <span>Author</span>
+      </div>
+    </>
+  );
+};
--- src/pages/questions/[questionId]/solutions/[solutionId].tsx
+++ src/pages/questions/[questionId]/solutions/[solutionId].tsx
@@ -1,16 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import { createProxySSGHelpers } from "@trpc/react/ssg";
-import clsx from "clsx";
 import {
   GetStaticPaths,
   GetStaticProps,
   GetStaticPropsContext,
   NextPage,
 } from "next";
-import { useSession } from "next-auth/react";
 import { ParsedUrlQuery } from "querystring";
-import { toast } from "react-hot-toast";
-import { FaUserAlt } from "react-icons/fa";
 import ReactMarkdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 import superjson from "superjson";
@@ -18,15 +14,71 @@ import superjson from "superjson";
 import { MarkdownImage } from "@/components/solution/MarkdownImage";
 import Divider from "@/components/ui/Divider";
 import PageHeader from "@/components/ui/PageHeader";
-import VoteIcon from "@/components/vote/VoteIcon";
+import { SolutionInfo } from "@/features/solution/components/solutionDetail/SolutionInfo";
 import { SolutionVote } from "@/features/solution/components/solutionDetail/SolutionVote";
-import { useUser } from "@/hooks/useUser";
 import { createContextInner } from "@/server/trpc/context";
 import { appRouter } from "@/server/trpc/router/_app";
-import { getTimeFromX } from "@/server/utils/dates";
-import { SOLUTION_VOTE_VALUE, SolutionVoteValue } from "@/types/solution-vote";
 import generateSolutionTitle from "@/utils/solution/generate-solution-title";
 import { trpc } from "@/utils/trpc";
+
+type PastPaperPageProps = {
+  solutionId: string;
+  questionId: string;
+  questionNumber: number;
+};
+
+const PastPaperPage: NextPage<PastPaperPageProps> = (props) => {
+  const { solutionId, questionId, questionNumber } = props;
+
+  const solution = trpc.solution.getOneById.useQuery({
+    id: solutionId,
+  });
+
+  const solutionName = generateSolutionTitle(solutionId);
+
+  if (!solution.isSuccess) return null;
+
+  return (
+    <div className="">
+      <div>
+        <PageHeader
+          title={`Solution of Question ${questionNumber}`}
+          className="mb-0"
+        />
+
+        <SolutionInfo
+          solutionName={solutionName}
+          authorId={solution.data.userId}
+          createdAt={solution.data.createdAt}
+        />
+        <Divider className="mt-2" />
+      </div>
+
+      <div className="flex w-full gap-4">
+        <div className="mt-2">
+          <SolutionVote
+            solutionId={solutionId}
+            questionId={questionId}
+            fallbackVoteCount={solution.data.votes}
+          />
+        </div>
+        <div className="mt-2 w-full">
+          <ReactMarkdown
+            rehypePlugins={[rehypeRaw]}
+            components={{
+              img: ({ src, alt }) => <MarkdownImage src={src!} alt={alt!} />,
+            }}
+            className="prose dark:prose-invert"
+          >
+            {solution.data.markdown.replace(/\n/, "  \n")}
+          </ReactMarkdown>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PastPaperPage;
 
 interface IParams extends ParsedUrlQuery {
   solutionId: string;
@@ -86,80 +138,3 @@ export const getStaticProps: GetStaticProps<
     // No need to revalidate, we don't have any dynamic data
   };
 };
-
-type PastPaperPageProps = {
-  solutionId: string;
-  questionId: string;
-  questionNumber: number;
-};
-
-const PastPaperPage: NextPage<PastPaperPageProps> = (props) => {
-  const { solutionId, questionId, questionNumber } = props;
-
-  const { user } = useUser();
-
-  const solution = trpc.solution.getOneById.useQuery({
-    id: solutionId,
-  });
-
-  const title = generateSolutionTitle(solutionId);
-
-  if (!solution.isSuccess) return null;
-
-  return (
-    <div className="">
-      <div>
-        <PageHeader
-          title={`Solution of Question ${questionNumber}`}
-          className="mb-0"
-        />
-
-        <p className="text-gray-400">
-          from <span className="text-gray-300">{title}</span>
-        </p>
-
-        <p className="text-gray-400">
-          posted{" "}
-          <span className="text-gray-300">
-            {getTimeFromX({ toDate: new Date(solution.data.createdAt) })}
-          </span>
-        </p>
-
-        <div
-          className={clsx(
-            user?.id !== solution.data.user.id && "hidden",
-            "flex items-center space-x-1 text-yellow-300"
-          )}
-        >
-          <FaUserAlt />
-          <span>You are the </span>
-          <span>Author</span>
-        </div>
-        <Divider className="mt-2" />
-      </div>
-
-      <div className="flex w-full gap-4">
-        <div className="mt-2">
-          <SolutionVote
-            solutionId={solutionId}
-            questionId={questionId}
-            fallbackVoteCount={solution.data.votes}
-          />
-        </div>
-        <div className="mt-2 w-full">
-          <ReactMarkdown
-            rehypePlugins={[rehypeRaw]}
-            components={{
-              img: ({ src, alt }) => <MarkdownImage src={src!} alt={alt!} />,
-            }}
-            className="prose dark:prose-invert"
-          >
-            {solution.data.markdown.replace(/\n/, "  \n")}
-          </ReactMarkdown>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default PastPaperPage;
