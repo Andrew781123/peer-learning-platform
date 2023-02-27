906a1cc188874d05e3a526de007861705f7cdfd1
Andrew781123 <leechihoandrew1123@gmail.com>
2023-02-27 21:59:17 +0800

    fix: clean up

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,15 +1,6 @@
 import * as Sentry from "@sentry/nextjs";
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
-console.log("SENTRY_DSN", SENTRY_DSN);
-
 Sentry.init({
   dsn: "https://e1383b9687ef4a07adf874d826f33db5@o4504745829203968.ingest.sentry.io/4504745829269504",
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
   tracesSampleRate: 1.0,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
 });
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -1,16 +1,6 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever middleware or an Edge route handles a request.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
 import * as Sentry from "@sentry/nextjs";
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
-
 Sentry.init({
-  dsn: SENTRY_DSN,
-  // Adjust this value in production, or use tracesSampler for greater control
+  dsn: "https://e1383b9687ef4a07adf874d826f33db5@o4504745829203968.ingest.sentry.io/4504745829269504",
   tracesSampleRate: 1.0,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
 });
