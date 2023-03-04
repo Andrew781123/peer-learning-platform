0054f9ade10e8fdc5d2864d26d1f55a12c347a1b
Andrew781123 <leechihoandrew1123@gmail.com>
2023-03-04 22:55:33 +0800

    debug: expose next public env

--- src/pages/auth/signin/index.tsx
+++ src/pages/auth/signin/index.tsx
@@ -92,7 +92,7 @@ const SignInPage: NextPage<SignInPageProps> = () => {
             type="button"
             primary
             className={clsx(
-              process.env.ENVIRONMENT === "production" && "hidden",
+              process.env.NEXT_PUBLIC_VERCEL_ENV === "production" && "hidden",
               "mt-2"
             )}
             isLoading={signInMutation.isLoading}
