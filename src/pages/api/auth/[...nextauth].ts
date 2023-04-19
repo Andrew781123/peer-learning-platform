34f6c0dc82b3ca39718539145cefc4281e92fdb4
Andrew781123 <leechihoandrew1123@gmail.com>
2023-04-19 12:16:23 +0800

    debug: log user agent

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -64,6 +64,8 @@ export const authOptions: NextAuthOptions = {
 const authHandler = NextAuth(authOptions);
 
 export default function auth(req: NextApiRequest, res: NextApiResponse) {
+  console.log("user agent", req.headers["user-agent"]);
+
   if (req.headers["x-vercel-ip-country"] !== "HK" || req.method === "HEAD") {
     return res.status(200).end();
   }
